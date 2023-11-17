65df9742b0b98202a50f8f6567080968f2bdc88a
Jakub Polczynski <jakub.polczynski00@gmail.com>
2023-11-17 19:13:36 +0100

    lab-d

---
+++ src/Form/MeasurementType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Measurement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class MeasurementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date', DateType::class, [
+                'widget' => 'single_text',
+                'html5' => true,
+            ])
+            ->add('celsius', NumberType::class, [
+                'html5' => true,
+            ])
+            ->add('location', EntityType::class, [
+                'class' => 'App\Entity\Location',
+                'choice_label' => 'city',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Measurement::class,
+        ]);
+    }
+}
