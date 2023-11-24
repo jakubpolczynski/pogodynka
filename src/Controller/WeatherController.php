26ac0b56b540e09398bc0987d2e65450c6049b54
Jakub Polczynski <jakub.polczynski00@gmail.com>
2023-11-24 18:40:28 +0100

    lab-e

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -7,14 +7,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Location;
 use App\Repository\MeasurementRepository;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class WeatherController extends AbstractController
 {
     #[Route('/weather/{city}/{country}', name: 'app_weather', requirements: ['id' => '\d+'])]
+    #[IsGranted('ROLE_WEATHER_CITY')]
     public function city(Location $location, MeasurementRepository $repository): Response
     {
         $measurements = $repository->findByLocation($location);
-        
+
         return $this->render('weather/city.html.twig', [
             'location' => $location,
             'measurements' => $measurements,
